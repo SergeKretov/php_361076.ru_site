dcd81125fa81be55423a9789eeb8b1b5c8e38977
SergeKretov <60303226+SergeKretov@users.noreply.github.com>
2020-01-28 08:25:18 +0700

    Create update_carda.php

---
+++ 361076.ru/ccard/update_carda.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="stylesheet" type="text/css" href="../style.css">
+<title>Обновление данных карты</title>
+</head>
+
+<body>
+<?php
+require 'connect.php';
+$active_date=$_REQUEST['active_date'];
+$active_date_end=trim($_REQUEST['active_date_end']);
+$active=trim($_REQUEST['active']);
+$cardnumber=trim($_REQUEST['cardnumber']);
+
+$update_sql = "UPDATE Cards SET active_date='$active_date', active_date_end='$active_date_end', active='$active' WHERE cardnumber='$cardnumber'";
+mysql_query($update_sql) or die("Ошибка вставки" . mysql_error());
+echo '<p>Запись успешно обновлена! </p>';
+echo $cardnumber;
+echo '</br>';
+?>
+<a href="http://www.361076.ru/ccard/search_card.html">Вернуться к поиску</a><br/><br/>
+<a href="http://www.361076.ru/ccard/select_change_card.php">Редактировать карту</a><br/><br/>
+<a href="http://www.361076.ru/ccard/addcard.html">Добавить новую карту</a><br/>
+<a href="http://www.361076.ru/ccard/activate_card.html">Активация карты клиента</a><br/>
+</body>
+</html>
