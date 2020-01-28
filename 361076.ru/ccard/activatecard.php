03bd1dd8a92bed6c8bca7d52bcf102bdeb8b7c28
SergeKretov <60303226+SergeKretov@users.noreply.github.com>
2020-01-28 08:28:47 +0700

    Create activatecard.php

---
+++ 361076.ru/ccard/activatecard.php
@@ -0,0 +1,31 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="stylesheet" href="style.css" type="text/css">
+<title>Выбор карты для активации.</title>
+</head>
+<body>
+<strong>Выбор карты для активации</strong>
+<form action="edit_carda.php" method="post">
+  <fieldset>
+<?php
+require 'connect.php';
+$select_sql = "SELECT * FROM Cards";
+$result = mysql_query($select_sql);
+$row = mysql_fetch_array($result);
+do
+{
+printf("<input type='radio' name='cardnum' value=" . $row['cardnumber'] . " id=" . $row['cardnumber'] . ">" . $row['cardnumber'] . "<br>");	
+}
+while($row = mysql_fetch_array($result));
+mysql_close();
+?>
+</fieldset>
+<fieldset>
+<input type="submit" value="Выбрать карту">
+</fieldset>
+</form>
+<a href="http://www.361076.ru/ccard/activate_card.html">Активация карты клиента</a><br/>
+</body>
+</html>
